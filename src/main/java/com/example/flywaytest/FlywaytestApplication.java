beb732c4a06fa69b7c0c15632f3f7b1c1934a95b
lasa84 <itprofessionals84@gmail.com>
2019-08-06 15:45:16 +0530

    initial commit

---
+++ src/main/java/com/example/flywaytest/FlywaytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.flywaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywaytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywaytestApplication.class, args);
+	}
+
+}
